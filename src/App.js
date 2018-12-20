daa015d8f14cc036c583a15576e0be5ff3f55e4e
Ben <benlittle53@gmail.com>
2018-12-21 12:43:28 +1300

    now recieving data back from API based on default searchQuery

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import Search from "./components/Search.js";
 import Button from "./components/Button.js";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    author: "Bob nmu",
-    url: "www.react.com",
-    comments: 4,
-    points: 3,
-    objectID: 1
-  },
-  {
-    title: "Vue",
-    author: "Rob nmu",
-    url: "www.vue.com",
-    comments: 2,
-    points: 5,
-    objectID: 2
-  }
-];
+// const list = [
+//   {
+//     title: "React",
+//     author: "Bob nmu",
+//     url: "www.react.com",
+//     comments: 4,
+//     points: 3,
+//     objectID: 1
+//   },
+//   {
+//     title: "Vue",
+//     author: "Rob nmu",
+//     url: "www.vue.com",
+//     comments: 2,
+//     points: 5,
+//     objectID: 2
+//   }
+// ];
 
 function isSearched(query) {
   return function(item) {
@@ -29,14 +29,39 @@ function isSearched(query) {
   };
 }
 
+const searchQuery = 'react';
+
+const NEWS_PATH = 'https://hn.algolia.com/api/v1';
+// const NEWS_SEARCH = './search';
+// const PARAM_SEARCH = 'query='
+const NEWS_URL = `${NEWS_PATH}/search?query=`
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list: list,
-      searchTerm: ""
+      // list: list,
+      // searchTerm: "",
+      result: null,
+      searchTerm: searchQuery
     };
+  }
+
+  topSearchStories = (result) => {
+    this.setState({
+      result
+    });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    
+    fetch(`${NEWS_URL}${searchTerm}`)
+      .then( res => res.json())
+      .then( data => this.topSearchStories(data) )
+      .catch(err => err);
+
   }
 
   onDismiss = id => {
@@ -52,15 +77,20 @@ class App extends Component {
   };
 
   render() {
-    const { searchTerm, list } = this.state;
+    console.log(this.state)
+    const { searchTerm, result } = this.state;
+    if(!result) return null
+
     return (
-      <div>
+      <div className="page">
+      <div className="interactions">
         <Search 
         value={searchTerm} 
         onChange={this.onSearchChange} 
         >Search</Search>
+        </div>
         <Table 
-        list={list} 
+        list={result.hits} 
         pattern={searchTerm} 
         onDismiss={this.onDismiss} 
         />
@@ -71,17 +101,19 @@ class App extends Component {
 
 const Table = ({ list, pattern, onDismiss }) => {
   return (
-    <div>
+    <div className="table">
         { list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
+          <div key={item.objectID} className="table__row">
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={ () => onDismiss(item.objectID) }>
+            <span style={{ width: '30%' }}>{item.author}</span>
+            <span style={{ width: '10%' }}>{item.comments}</span>
+            <span style={{ width: '10%' }}>{item.points}</span>
+            <span style={{ width: '10%' }}>
+              <Button onClick={ () => onDismiss(item.objectID) }
+              className="button__inline"
+              >
                 Dismiss
               </Button>
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/Search.css';
 
 const Search = ({ value, onChange, children }) => {
   return (
